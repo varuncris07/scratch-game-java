e17b3f7754cf3851c9da55864af9f272c52d3ca8
varun <vartun.tiwari@gmail.com>
2025-04-23 13:49:00 +0530

    adding logic

--- src/main/java/com/scratch/model/ProbabilityConfig.java
+++ src/main/java/com/scratch/model/ProbabilityConfig.java
@@ -1,17 +1,3 @@
-//package com.scratch.model;
-//
-//import java.util.List;
-//
-//public class ProbabilityConfig {
-//    private List<StandardSymbolProbability> standardSymbols;
-//    private BonusSymbolProbability bonusSymbols;
-//
-//    public List<StandardSymbolProbability> getStandardSymbols() { return standardSymbols; }
-//    public void setStandardSymbols(List<StandardSymbolProbability> standardSymbols) { this.standardSymbols = standardSymbols; }
-//
-//    public BonusSymbolProbability getBonusSymbols() { return bonusSymbols; }
-//    public void setBonusSymbols(BonusSymbolProbability bonusSymbols) { this.bonusSymbols = bonusSymbols; }
-//}
 package com.scratch.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
